8174bab46b61a67e27ed77d30b2d2da47be844c0
verdiow <140170309+VerdioWong@users.noreply.github.com>
2024-06-04 22:15:19 +0800

    Update Square.jsx

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -3,7 +3,7 @@ import '../tailwind.css';
 import cross from '../assets/cross.svg';
 import circle from '../assets/circle.svg';
 
-const Square = ({ svg, onClick, additionalClasses }) => {
+const Square = ({ svg, onClick, additionalClasses, index}) => {
   const renderSVG = () => {
     if (svg === "cross") {
       return <img src={cross} alt="Cross" className="w-12 h-12 mx-auto p-0" />;
@@ -15,6 +15,7 @@ const Square = ({ svg, onClick, additionalClasses }) => {
 
   return (
     <button
+      data-testid={`square-${index}`}
       className={`border-4 border-white dark:border-white ${!svg ? 'p-7' : 'p-1'} ${additionalClasses} hover:bg-sky-400/30 dark:hover:bg-sky-600/30`}
       onClick={onClick}
       disabled={!!svg} // Disable the button if the square is filled
